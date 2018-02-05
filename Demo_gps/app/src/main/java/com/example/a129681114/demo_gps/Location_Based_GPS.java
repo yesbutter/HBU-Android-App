cc3126c82d124029156b02a1f120f743b9f62c24
T-Rex <1296821114@qq.com>
2018-02-05 20:12:33 +0800

    做了一个使用GPS得出来经纬度的小demo，还有很多问题，等待完善！

---
+++ Demo_gps/app/src/main/java/com/example/a129681114/demo_gps/Location_Based_GPS.java
@@ -0,0 +1,17 @@
+package com.example.a129681114.demo_gps;
+
+import android.location.Location;
+
+/**
+ * Created by 12968 on 2018/2/5.
+ */
+
+public class Location_Based_GPS {
+
+
+
+    private void upLoadInfo(Location location)
+    {
+
+    }
+}
