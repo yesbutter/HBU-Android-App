889cc636e26f3698206887766dafd96acefc9e80
yesbutter <1296821114@qq.com>
2018-02-13 18:38:30 +0800

    想做一个简单下滑，发现没这么简单！！！后续代码还需继续完善。目前实现效果下拉到一定地方松开可以实现加载画面！

--- ActivitySwitchinganimation/app/src/main/java/com/example/a12968/activityswitchinganimation/SecondActivity.java
+++ ActivitySwitchinganimation/app/src/main/java/com/example/a12968/activityswitchinganimation/SecondActivity.java
@@ -1,9 +1,12 @@
 package com.example.a12968.activityswitchinganimation;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import jp.co.recruit_lifestyle.android.widget.WaveSwipeRefreshLayout;
 
 /**
  * Created by 12968 on 2018/2/12.
@@ -14,12 +17,32 @@ public class SecondActivity extends Activity {
     Button button;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_activity);
-        button=findViewById(R.id.back_button);
+
+        setContentView(R.layout.refresh_layout);
+        initView();
+
+       // button=findViewById(R.id.back_button);
     }
 
-    public void backclick(View view)
+    private void initView()
     {
-        finish();
+        WaveSwipeRefreshLayout mWaveSwipeRefreshLayout = (WaveSwipeRefreshLayout) findViewById(R.id.my_wave_swipe);
+        mWaveSwipeRefreshLayout.setOnRefreshListener(new WaveSwipeRefreshLayout.OnRefreshListener() {
+            @Override public void onRefresh() {
+                // Do work to refresh the list here.
+                //new Task().execute();
+            }
+        });
     }
+
+    private class Task {
+        public void execute() {
+        }
+    }
+
+
+//    public void backclick(View view)
+//    {
+//        finish();
+//    }
 }
